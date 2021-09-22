934070422c4a86880d446339fa0f0d1c27a83cdd
practicalcoderyt <90580813+practicalcoderyt@users.noreply.github.com>
2021-09-22 12:02:55 -0400

    Update naming.py

--- naming.py
+++ naming.py
@@ -38,5 +38,52 @@ def get_flagged_cells(gameboard):
 	for cell in gameboard:
 		if cell.is_flagged():
 			flagged_cells.append(cell)
-	return flagged_cells		
+	return flagged_cells
+
+
+
+	# consider
+	def copy_string(a1, a2)
 	
+	# vs
+	def copy_string(source, destination)
+	
+	
+	# how can you tell the difference in these function calls by how they are named?
+	get_account()
+	get_accounts()
+	get_account_info()
+	# a renaming of these functions is almost necessary for other programmers to use them
+	
+	
+	# use searchable names
+	WORK_DAYS_PER_WEEK = 5
+	# it is much easier to searh for WORK_DAYS_PER_WEEK than it is to search for the number 5
+	
+	# forget about prefixes, consider the two ways to name this string variable of Part
+	class Part:
+		__init__(self, description):
+			self.m_desc = description
+			self.description = description
+			
+			
+	# classes should be nouns, functions should be verbs
+	first_floor = Blueprint()
+	scan_blueprint(first_floor)
+	
+	# say what you mean
+	whack() vs kill()
+	gtfo() vs abort()
+	
+	# don't use the same thing for different things, consider
+	Cumulator.add(num)
+	Accounts.add(account)
+	# one of these should be renamed so that they aren't confused. A good option is to rename Accounts.add() to
+	Accounts.append(account)
+	
+	# Add context to your variables, consider
+	state
+	addrState
+	sysState
+	address.state
+	system.state
